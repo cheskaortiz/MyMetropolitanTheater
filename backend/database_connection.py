d74aca4da26657c932dfc9ba0d585de72b121a3c
achctonin <ninotchca.sollestre@tup.edu.ph>
2026-05-18 15:43:49 +0800

    updated ticket repo

--- backend/database_connection.py
+++ backend/database_connection.py
@@ -2,9 +2,9 @@ import psycopg2
 def get_connection():
     try:
         conn = psycopg2.connect(
-            database="MyMetropolitanTheater", #change to your created database
+            database="DatabaseTester", #change to your created database
             user="postgres",
-            password="Sherlin21@@@", #change to your own password
+            password="Chuchay_0926", #change to your own password
             host="127.0.0.1",
             port=5432,
         )
